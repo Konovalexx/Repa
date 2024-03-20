3fa69d5afcacdd7d828e1917fb8b350d78e55fd8
Alex Con <konovalexx@gmail.com>
2024-03-20 23:01:22 +1000

    Add def capitalize_first_letter to str_func.py and docstrings to def to_uppercase

--- str_func.py
+++ str_func.py
@@ -1,2 +1,15 @@
 def to_uppercase(s):
+    """
+    Вроде бы, я забыл написать докстринги в прошлый раз
+    """
     return s.upper()
+
+
+def capitalize_first_letters(s):
+    """
+    Преобразует первую букву каждого слова в строке в заглавную.
+
+    :param s: Входная строка
+    :return: Строка с заглавными первыми буквами слов
+    """
+    return ' '.join(word.capitalize() for word in s.split())
